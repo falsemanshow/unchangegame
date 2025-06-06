78f20e13c2c086bbaac5c9fa314577a89ca33d4b
falsemanshow <martus.kheylavalero@gmail.com>
2025-06-06 21:51:22 +0800

    Update mainwhole.js

--- mainwhole.js
+++ mainwhole.js
@@ -30,12 +30,11 @@ function updateBlocking(p, pid) {
   }
   p._wasBlocking = p.blocking;
 
-  if (p.dizzy > 0) {
-    p.dizzy--;
-    p.vx *= FRICTION;
-    if (Math.abs(p.vx) < 0.3) p.vx = 0;
-    return true;
-  }
+ if (p.dizzy > 0) {
+  p.dizzy--;
+
+  return true;
+}
   return false;
 }
 
